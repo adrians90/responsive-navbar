dcbf8aef0be67e3a6cad0e7edd96c366e03cb27b
adrians90 <adriansandu1990@yahoo.com>
2023-05-19 14:29:16 +0100

    wrote a script that toggles the hamburger menu dropping down the links when clicked(only on mobile device screens)

---
+++ script.js
@@ -0,0 +1,9 @@
+//when pressed, the hamburger menu drops down the list of navbar links,
+//when clicked again, removes them
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
